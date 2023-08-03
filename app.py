e6c89654aaaf0b5607ad336ced17c947988b8c60
Emily X <40601891+emilyxietty@users.noreply.github.com>
2023-08-03 13:32:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -58,20 +58,29 @@ def main():
     if drawing_mode == 'point':
         point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
     stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-    bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-    bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-    
+    bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")    
     realtime_update = st.sidebar.checkbox("Update in realtime", True)
     
     
     
     # Create a canvas component
+    # canvas_result = st_canvas(
+    #     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    #     stroke_width=stroke_width,
+    #     stroke_color=stroke_color,
+    #     background_color=bg_color,
+    #     background_image=Image.open(bg_image) if bg_image else None,
+    #     update_streamlit=realtime_update,
+    #     height=150,
+    #     drawing_mode=drawing_mode,
+    #     point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+    #     key="canvas",
+    # )
     canvas_result = st_canvas(
         fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
         stroke_width=stroke_width,
         stroke_color=stroke_color,
         background_color=bg_color,
-        background_image=Image.open(bg_image) if bg_image else None,
         update_streamlit=realtime_update,
         height=150,
         drawing_mode=drawing_mode,
