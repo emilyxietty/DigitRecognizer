de986653d983da1b285055f2e7fd586ca568d3a9
Emily X <40601891+emilyxietty@users.noreply.github.com>
2023-08-03 13:22:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,8 +49,37 @@ def open_img(drawn_image):
 def main():
     print_title()
 
+    # Specify canvas parameters in application
+    drawing_mode = st.sidebar.selectbox(
+        "Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
+    )
+    
+    stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+    if drawing_mode == 'point':
+        point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
+    stroke_color = st.sidebar.color_picker("Stroke color hex: ")
+    bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+    bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
+    
+    realtime_update = st.sidebar.checkbox("Update in realtime", True)
+    
+    
+    
+    # Create a canvas component
+    canvas_result = st_canvas(
+        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        background_image=Image.open(bg_image) if bg_image else None,
+        update_streamlit=realtime_update,
+        height=150,
+        drawing_mode=drawing_mode,
+        point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+        key="canvas",
+    )
     # Create a canvas to draw on
-    canvas_result = st_canvas(fill_color="rgb(200, 200, 200)", stroke_width=5, stroke_color="rgb(0, 0, 0)", update_streamlit=True, height=200)
+    # canvas_result = st_canvas(fill_color="rgb(200, 200, 200)", stroke_width=5, stroke_color="rgb(0, 0, 0)", update_streamlit=True, height=200)
 
     # Display the image drawn on the canvas
     if canvas_result.image_data is not None:
