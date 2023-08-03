af6771d214793270154f6aef0fa78ea4f00c9421
Emily X <40601891+emilyxietty@users.noreply.github.com>
2023-08-03 13:48:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -87,13 +87,11 @@ def main():
     canvas_result = st_canvas(
         fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
         stroke_width=stroke_width,
-        stroke_color=stroke_color,
         background_color=bg_color,
-        background_image=Image.open(bg_image) if bg_image else None,
         update_streamlit=realtime_update,
         height=150,
         drawing_mode=drawing_mode,
-        point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
+        point_display_radius=point_display_radius if drawing_mode == 'freedraw' else 0,
         key="canvas",
     )
 
